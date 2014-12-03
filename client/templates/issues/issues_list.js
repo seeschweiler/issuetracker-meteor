a89accddc33b0cebf43ca039321b31967584485b
Sebastian Eschweiler <sebastian@meanstack.de>
2014-12-03 19:22:23 +0100

    second commit

--- client/templates/issues/issues_list.js
+++ client/templates/issues/issues_list.js
@@ -1,23 +1,5 @@
-var issueData = [
-  {
-    title: 'First Issue Title',
-    description: 'First Issue Description',
-    dueDate: '2015-01-01',
-    priority: 'Low'
-  },
-  {
-    title: 'Second Issue Title',
-    description: 'Second Issue Description',
-    dueDate: '2015-01-02',
-    priority: 'Medium'
-  },
-  {
-    title: 'Third Issue Title',
-    description: 'Third Issue Description',
-    dueDate: '2015-01-03',
-    priority: 'High'
-  }
-];
 Template.issuesList.helpers({
-  issues: issueData
+  issues: function() {
+    return Issues.find();
+  }
 });
